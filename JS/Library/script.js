0ffea581b978b5eab898eabd208f74f4fa8d484f
Trephy <110808132+Trephyy@users.noreply.github.com>
2022-10-23 00:36:48 +0300

    Library UNFINISHED 0.0.1

--- JS/Library/script.js
+++ JS/Library/script.js
@@ -2,8 +2,17 @@ let myLibrary = [];
 const root = document.querySelector(':root')
 const currentColor = getComputedStyle(root).getPropertyValue("--theme-color")
 const theme1 = document.getElementById('dark');
-
 const theme2 = document.getElementById('light');
+const cancelButton = document.getElementById('Cancel')
+const addNew = document.getElementById('addNew')
+const newBookPopup = document.getElementById('newBookWindow')
+
+addNew.onclick = () =>{
+      newBookPopup.style.display = "block"; 
+}
+cancelButton.onclick = () =>{
+  newBookPopup.style.display = "none";
+}
 theme1.onclick = () => {
     themeSetDark()
     theme1.style.display = "none"
@@ -17,16 +26,17 @@ theme2.onclick = () => {
 function themeSetLight(){
     root.style.setProperty('--theme-color', 'skyblue')
     root.style.setProperty('--theme-text-color', 'black')
+    root.style.setProperty('--theme-sidebar-color', 'wheat')
 }
 function themeSetDark(){
     root.style.setProperty('--theme-color', '#101010')
     root.style.setProperty('--theme-text-color', 'white')
+    root.style.setProperty('--theme-sidebar-color', 'rgb(70, 70, 70)')
 }
+function isInputNumber (evt){
+  const ch = String.fromCharCode(evt.which);
+  if (!(/\d{1,6}/.test(ch))){
 
-function Book() {
-  // the constructor...
-}
-
-function addBookToLibrary() {
-  // do stuff here
+    evt.preventDefault();
+  }
 }
